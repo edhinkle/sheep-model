a976b92841b7dc0c29faca5055e7849c648d85cc
Elise Dianne Hinkle <ehinkle@login29.chn.perlmutter.nersc.gov>
2025-06-18 07:29:49 -0700

    Adding scripts to convert edepsim ROOT to hdf5 and display edepsim events from hdf5 files.

--- sample_gen/make_edep_sim_sample.sh
+++ sample_gen/make_edep_sim_sample.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 GEOMETRY=$1
-OUTPUT=$2
+EDEP_FILE=$2
 NEVENTS=$3
 EDEP_MACRO=$4
 
@@ -9,6 +9,14 @@ export CPATH=$EDEPSIM/include/EDepSim:$CPATH
 
 edep-sim \
     -g ${GEOMETRY} \
-    -o ${OUTPUT} \
+    -o ${EDEP_FILE} \
     -e ${NEVENTS} \
     ${EDEP_MACRO}
+
+
+H5_FILE=${EDEP_FILE%.root}.hdf5
+
+python3 single_shower_convert_edepsim_roottoh5.py ${EDEP_FILE} ${H5_FILE} 
+
+#mv ${EDEP_FILE} ${OUTDIR}/edep/${EDEP_FILE}
+#mv ${H5_FILE} ${OUTDIR}/h5df/${H5_FILE}
