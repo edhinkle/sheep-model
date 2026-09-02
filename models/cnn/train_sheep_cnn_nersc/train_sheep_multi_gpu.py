5829a2fe657234916dfb816580d4ad4bf9b36257
Elise Dianne Hinkle <ehinkle@login07.chn.perlmutter.nersc.gov>
2026-09-02 08:18:08 -0700

    Modifying training so that targets are visibility fraction vs. true energy and adding weight decay/training speed-up changes.

--- models/cnn/train_sheep_cnn_nersc/train_sheep_multi_gpu.py
+++ models/cnn/train_sheep_cnn_nersc/train_sheep_multi_gpu.py
@@ -328,9 +328,10 @@ class Trainer():
             #idxs = []
 
         end_of_last_step = time.time()
-        for i, (inputs, targets, VE_frac, MG_frac, OOB_frac, start_pos, rot_mat, idx) in enumerate(self.train_data_loader):
+        for i, (inputs, KE_init, VE_frac, MG_frac, OOB_frac, start_pos, rot_mat, idx) in enumerate(self.train_data_loader):
             self.iters += 1
             #print("Inputs: ", inputs.size())
+            targets = VE_frac
             inputs, targets = inputs.to(self.device, non_blocking=True), targets.to(self.device, non_blocking=True)
             #print("Active pixels:",inputs.shape[0])
             tr_start = time.time()
@@ -438,7 +439,8 @@ class Trainer():
             #idxs = []
 
         with torch.no_grad():
-            for i, (inputs, targets, VE_frac, MG_frac, OOB_frac, start_pos, rot_mat, idx) in enumerate(self.val_data_loader):
+            for i, (inputs, KE_init, VE_frac, MG_frac, OOB_frac, start_pos, rot_mat, idx) in enumerate(self.val_data_loader):
+                targets = VE_frac
                 inputs, targets = inputs.to(self.device, non_blocking=True), targets.to(self.device, non_blocking=True)
                 outputs = self.model(inputs)
                 loss = self.loss_func(outputs, targets)
