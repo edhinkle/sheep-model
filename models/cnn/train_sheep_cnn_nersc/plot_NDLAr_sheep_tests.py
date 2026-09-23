ce0d33ecf9ff88c8d9db5e1a8defdcd1c6375dd4
Elise Hinkle <52926703+edhinkle@users.noreply.github.com>
2026-09-23 09:06:37 -0500

    Add energy metrics to CSV data processing in ND-LAr Test Plots script

--- models/cnn/train_sheep_cnn_nersc/plot_NDLAr_sheep_tests.py
+++ models/cnn/train_sheep_cnn_nersc/plot_NDLAr_sheep_tests.py
@@ -113,6 +113,11 @@ class TestedSheepNDLAr():
         ve_frac = []
         mg_frac = []
         oob_frac = []
+        minE = []
+        maxE = []
+        meanE = []
+        medE = []
+        numVox = []
         start_position = []
         rotation_matrix = []
         with open(self.csv_file, 'r') as f:
@@ -125,6 +130,11 @@ class TestedSheepNDLAr():
                 ve_frac.append(float(row['ve_frac']))
                 mg_frac.append(float(row['mg_frac']))
                 oob_frac.append(float(row['oob_frac']))
+                minE.append(float(row['minVoxE']))
+                maxE.append(float(row['maxVoxE']))
+                meanE.append(float(row['meanVoxE']))
+                medE.append(float(row['medianVoxE']))
+                numVox.append(float(row['numFilledVoxels']))
                 start_position.append(str(row['start_position']))
                 rotation_matrix.append(str(row['rotation_matrix']))
 
@@ -134,6 +144,11 @@ class TestedSheepNDLAr():
         self.ve_frac = np.array(ve_frac)
         self.mg_frac = np.array(mg_frac)
         self.oob_frac = np.array(oob_frac)
+        self.minE = np.array(minE)
+        self.maxE = np.array(maxE)
+        self.meanE = np.array(meanE)
+        self.medE = np.array(medE)
+        self.numVox = np.array(numVox)
         self.start_position = np.array([np.fromstring(s.strip('[]'), sep=' ') for s in start_position])
         self.rotation_matrix = np.array([
             np.fromstring(r.replace('\n', ' ').replace('[', '').replace(']', ''), sep=' ').reshape(3, 3)
